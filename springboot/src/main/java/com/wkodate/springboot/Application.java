0552be0df77f7b92e32f0e67e8c255605ef4f7bf
wkodate <hbwandeow@gmail.com>
2018-11-03 11:41:21 +0900

    prepared to launch

--- springboot/src/main/java/com/wkodate/springboot/Application.java
+++ springboot/src/main/java/com/wkodate/springboot/Application.java
@@ -1,22 +1,20 @@
 package com.wkodate.springboot;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by wkodate on 2018/10/30.
  */
-@RestController
-@EnableAutoConfiguration
-public class Example {
+@SpringBootApplication
+public class Application {
     @RequestMapping("/")
     String home() {
         return "Hello World!";
     }
 
     public static void main(String[] args) throws Exception {
-        SpringApplication.run(Example.class, args);
+        SpringApplication.run(Application.class, args);
     }
 }
