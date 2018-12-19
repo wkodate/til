fba0a49a298e59ebef24264ad2b47b2cfe8c6547
wkodate <hbwandeow@gmail.com>
2018-12-20 02:10:50 +0900

    try how to deploy spring boot application with docker

---
+++ docker/springboot/src/test/java/com/wkodate/dockertest/DockerTestApplicationTests.java
@@ -0,0 +1,17 @@
+package com.wkodate.dockertest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DockerTestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
