5f337f0e198502415daa2ff721c94fc4c8ddb6d3
wkodate <hbwandeow@gmail.com>
2019-01-22 01:44:43 +0900

    try "Explore InSpec resources"

---
+++ chef/learn-inspec/my_nginx/controls/example.rb
@@ -0,0 +1,57 @@
+# encoding: utf-8
+# copyright: 2018, The Authors
+
+title 'sample section'
+
+# you can also use plain tests
+describe file('/tmp') do
+  it { should be_directory }
+end
+
+# you add controls here
+control 'tmp-1.0' do                        # A unique ID for this control
+  impact 0.7                                # The criticality, if this control fails.
+  title 'Create /tmp directory'             # A human-readable title
+  desc 'An optional description...'
+  describe file('/tmp') do                  # The actual test
+    it { should be_directory }
+  end
+end
+
+nginx_params = yaml(content: inspec.profile.file('params.yml')).params
+
+required_version = nginx_params['version']
+required_modules = nginx_params['modules']
+
+control 'nginx-version' do
+  impact 1.0
+  title 'NGINX version'
+  desc 'The required version of NGINX should be installed.'
+  describe nginx do
+    its('version') { should cmp >= required_version }
+  end
+end
+
+control 'nginx-modules' do
+  impact 1.0
+  title 'NGINX version'
+  desc 'The required NGINX modules should be installed.'
+  describe nginx do
+    required_modules.each do |required_module|
+      its('modules') { should include required_module }
+    end
+  end
+end
+
+control 'nginx-conf' do
+  impact 1.0
+  title 'NGINX configuration'
+  desc 'The NGINX config file should owned by root, be writable only by owner, and not writeable or and readable by others.'
+  describe file('/etc/nginx/nginx.conf') do
+    it { should be_owned_by 'root' }
+    it { should be_grouped_into 'root' }
+    it { should_not be_readable.by('others') }
+    it { should_not be_writable.by('others') }
+    it { should_not be_executable.by('others') }
+  end
+end
