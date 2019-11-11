deed03bae9b213acfa4379e61907a109ac951ddf
wkodate <hbwandeow@gmail.com>
2019-11-11 14:58:42 +0900

    update Gemfiles

--- ruby-on-rails/taskapp/app/models/task.rb
+++
@@ -1,5 +0,0 @@
-class Task < ActiveRecord::Base
-  belongs_to :project
-  validates :title, presence: true
-  scope :unfinished, -> { where(done: false) }
-end
--- ruby-on-rails/taskapp/db/migrate/20150926083611_create_projects.rb
+++
@@ -1,9 +0,0 @@
-class CreateProjects < ActiveRecord::Migration
-  def change
-    create_table :projects do |t|
-      t.string :title
-
-      t.timestamps null: false
-    end
-  end
-end
