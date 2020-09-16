6be803afaf14e2ea75951316717a76723395c7b3
wkodate <hbwandeow@gmail.com>
2020-09-17 00:57:29 +0900

    create flink project for elasticsearch connector

---
+++ flink/src/main/java/com/wkodate/flink/ElasticsearchInputSample.java
@@ -0,0 +1,9 @@
+package com.wkodate.flink;
+
+public class ElasticsearchInputSample {
+
+    public static final String[] DATA = new String[]{
+            "{\"name\":\"wataru\", \"age\":32, \"subjects\":[\"math\",\"english\"]}",
+    };
+
+}
