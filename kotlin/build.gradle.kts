e1c2cb1c9d1c180acbb5bf95f43fff49014d3d95
wkodate <hbwandeow@gmail.com>
2021-01-16 23:57:01 +0900

    learn kotlin

--- kotlin/build.gradle.kts
+++ kotlin/build.gradle.kts
@@ -8,10 +8,20 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    maven("https://dl.bintray.com/kotlin/kotlin-eap")
 }
 dependencies {
     testImplementation(kotlin("test-junit"))
+    implementation(kotlin("stdlib-jdk8"))
 }
 tasks.withType<KotlinCompile>() {
     kotlinOptions.jvmTarget = "1.8"
+}
+val compileKotlin: KotlinCompile by tasks
+compileKotlin.kotlinOptions {
+    jvmTarget = "1.8"
+}
+val compileTestKotlin: KotlinCompile by tasks
+compileTestKotlin.kotlinOptions {
+    jvmTarget = "1.8"
 }
---
+++ kotlin/src/main/kotlin/com/wkodate/learnkotlin/introduction/HelloWorld.kt
@@ -0,0 +1,9 @@
+package com.wkodate.learnkotlin.introduction
+
+class HelloWorld {
+
+    fun main() {
+        println("Helo, World")
+    }
+
+}
